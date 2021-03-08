a5c54249d4a3c3a1a5b766dc016e8ac55ebfb0e2
IhorTarkhan <igor.tark@gmail.com>
2021-03-08 20:16:27 +0200

    fade effect

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
-const timeoutInSecond = 4;
 let slideIndex = 0;
+/*
+const timeoutInSecond = 2;
 const slides = [...document.getElementsByClassName("slide")];
 carousel();
 function carousel() {
@@ -8,3 +9,22 @@ function carousel() {
   slideIndex = (slideIndex + 1) % slides.length;
   setTimeout(carousel, timeoutInSecond * 1000);
 }
+*/
+
+const imgArray = [
+  "http://placehold.it/300x200",
+  "http://placehold.it/200x100",
+  "http://placehold.it/400x300"
+];
+const imgDuration = 2000;
+
+function slideShow() {
+  document.getElementById("slider").classList.add("fadeOut");
+  setTimeout(() => {
+    document.getElementById("slider").src = imgArray[slideIndex];
+    document.getElementById("slider").classList.remove("fadeOut");
+  }, 1000);
+  slideIndex = (slideIndex + 1) % imgArray.length;
+  setTimeout(slideShow, imgDuration);
+}
+slideShow();
