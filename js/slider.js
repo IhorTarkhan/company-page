57dce294b91e08165d3f936dbf5f73f515d566d8
IhorTarkhan <ihor.lafox.net>
2021-03-27 13:26:45 +0200

    slider refactored

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,23 @@
 let slideIndex = 0;
-/*
-const timeoutInSecond = 2;
-const slides = [...document.getElementsByClassName("slide")];
-carousel();
-function carousel() {
-  slides.forEach(slide => (slide.style.display = "none"));
-  slides[slideIndex].style.display = "block";
-  slideIndex = (slideIndex + 1) % slides.length;
-  setTimeout(carousel, timeoutInSecond * 1000);
-}
-*/
+const imgArray = [...document.getElementsByClassName("slide")];
 
-const imgArray = [
-  "http://placehold.it/300x200",
-  "http://placehold.it/200x100",
-  "http://placehold.it/400x300"
-];
-const imgDuration = 2000;
+const animationTime = 500;
+const callbackTimeout = 50;
+const imgDuration = 4000;
 
 function slideShow() {
-  document.getElementById("slider").classList.add("fadeOut");
+  imgArray.forEach(it => (it.style.display = "none"));
+  imgArray[slideIndex].style.display = "block";
   setTimeout(() => {
-    document.getElementById("slider").src = imgArray[slideIndex];
-    document.getElementById("slider").classList.remove("fadeOut");
-  }, 1000);
-  slideIndex = (slideIndex + 1) % imgArray.length;
-  setTimeout(slideShow, imgDuration);
+    imgArray[slideIndex].classList.remove("fadeOut");
+  }, callbackTimeout);
+
+  setTimeout(() => {
+    imgArray[slideIndex].classList.add("fadeOut");
+    slideIndex = (slideIndex + 1) % imgArray.length;
+    imgArray[slideIndex].classList.add("fadeOut");
+    setTimeout(slideShow, animationTime + callbackTimeout);
+  }, imgDuration - (animationTime + 2 * callbackTimeout));
 }
+
 slideShow();
