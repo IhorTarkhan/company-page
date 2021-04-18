e118375f24d0a4bc37f2c57ddccf03edeae0b12f
IhorTarkhan <ihor.lafox.net>
2021-04-18 14:39:44 +0300

    popup rework

--- js/feedback.js
+++
@@ -1,9 +0,0 @@
-const popupWrapper = document.getElementById("content-feedback");
-
-const openPopup = () => {
-  popupWrapper.classList.remove("hide");
-};
-popupWrapper.addEventListener("click", (e) => {
-  if (e.target !== e.currentTarget) return;
-  popupWrapper.classList.add("hide");
-});
